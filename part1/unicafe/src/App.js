f25f7fa348cbb547dad1da86f94a230fac4ae763
LakshithaV <2018csc017@univ.jfn.ac.lk>
2021-10-05 22:47:05 +0530

    1.6: unicafe step1 - Update

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
-
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
       <h1>statistics</h1>
       <p>
         good {good} <br />
