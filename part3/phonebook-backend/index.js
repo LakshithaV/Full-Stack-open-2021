a20e05cfd8562ad1d07bd20883456e3d1db94e35
LakshithaV <2018csc017@univ.jfn.ac.lk>
2021-10-10 13:35:10 +0530

    3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -32,6 +32,13 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const content = `Phonebook has info for ${persons.length} people
+    <br/><br/>
+    ${new Date()}`;
+  response.send(content);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
